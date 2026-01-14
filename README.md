0ab370b30977179de85217fda9562ea1f5d2bd5a
p1k0chu <p1k0chu.vim@gmail.com>
2026-01-14 21:46:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# unicode tags converter
+
+converts your text into zero-width characters associated with the letters.  
+those are in unicode range E0000-EFFFF and each ascii symbol has a zero-width version  
+they are completely invisible, you could use this for any reason
+
---
+++ index.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+    var input = document.getElementById("input");
+    var output = document.getElementById("output");
+}
+
+function get_action() {
+    return document.querySelector('input[name="action"]:checked').value;
+}
+
+function convert() {
+    const action = get_action();
+    console.log(action)
+
+    const s = input.value;
+    let out = "";
+    for (const chr of s) {
+        const code = chr.codePointAt(0);
+
+        let chr2;
+        if (action == "encode") {
+            chr2 = String.fromCodePoint(code | 0xe0000);
+        } else if (action == "decode") {
+            chr2 = String.fromCodePoint(code & ~(0xe0000));
+        }
+        out += chr2;
+    }
+    output.value = out;
+}
+
+function copy() {
+    output.select();
+    output.setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText(output.value);
+}
+
